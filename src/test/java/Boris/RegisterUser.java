737c7f7cffb7a5ec73c49c7878404125df0d9abf
Boris1329 <127370557+Boris1329@users.noreply.github.com>
2023-05-08 19:17:55 -0400

    5 Selenium test cases

---
+++ src/test/java/Boris/RegisterUser.java
@@ -0,0 +1,81 @@
+package Boris;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import us.piit.SetUp;
+
+import java.util.Random;
+
+public class RegisterUser extends SetUp {
+
+    @FindBy(xpath = "(//a[text()='Create an Account'])[1]")
+    WebElement CreateAnAccountLink;
+
+    @FindBy(id = "firstname")
+    WebElement FirstName;
+
+    @FindBy(id = "lastname")
+    WebElement LastName;
+
+    @FindBy(id = "email_address")
+    WebElement EmailInput;
+
+    @FindBy(id = "password")
+    WebElement PasswordInput;
+
+    @FindBy(id = "password-confirmation")
+    WebElement ConfirmPassword;
+
+    @FindBy(xpath = "//button[@class=\"action submit primary\"]")
+    WebElement CreateAccountButton;
+    
+    @FindBy(xpath = "//div[@class=\"box box-information\"]//div[@class=\"box-content\"]")
+    WebElement UserInfo;
+
+    @FindBy(xpath = "//div[text()='Thank you for registering with Main Website Store.']")
+    WebElement WelcomeMessage;
+
+    String TestEmail = generateTestEmail();
+
+    @Parameters({"Password"})
+    @Test
+    public void CreateAnAccount(String Password){
+
+        // Verify that user is on Home page
+        Assert.assertEquals(getCurrentTitle(), "Home Page");
+        isVisible(CreateAnAccountLink);
+
+        //click on Create account button
+        CreateAnAccountLink.click();
+
+        // function which actually fill the registration form
+        CreateNewUser(TestEmail, Password);
+        waitFor(1);
+        CreateAccountButton.click();
+        waitFor(2);
+        isVisible(UserInfo);
+        Assert.assertTrue(UserInfo.getText().contains(TestEmail));
+        Assert.assertTrue(WelcomeMessage.isDisplayed());
+        Assert.assertEquals(WelcomeMessage.getText(), "Thank you for registering with Main Website Store.");
+
+    }
+
+
+
+
+    // Create user function
+    public void CreateNewUser(String Email, String Password){
+
+        Assert.assertTrue(FirstName.isDisplayed());
+
+        FirstName.sendKeys("Boris");
+        LastName.sendKeys(".");
+        EmailInput.sendKeys(TestEmail);
+        PasswordInput.sendKeys(Password);
+        ConfirmPassword.sendKeys(Password);
+    }
+
+}
