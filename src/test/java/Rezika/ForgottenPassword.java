e67674640fafd1716191bf0748e39d202b2e1b63
Rezikabareche <127370359+Rezikabareche@users.noreply.github.com>
2023-05-09 15:06:35 -0400

    Selenium test cases completed

---
+++ src/test/java/Rezika/ForgottenPassword.java
@@ -0,0 +1,129 @@
+package com.ninjaTutorials;
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import us.piit.SetUp;
+
+public class ForgottenPassword  extends SetUp {
+
+
+    Logger log = LogManager.getLogger(ForgottenPassword.class.getName());
+
+
+    //verify if the user is able to reset the password
+    @Test
+    public void restUserPassword() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the forgot password page
+        clickOn("//a[normalize-space()='Forgotten Password']");
+        log.info("Clicked on the 'Forgotten Password' link success");
+
+        // Enter the email and submit
+        type("#input-email","sunyellow@gmail.com" );
+        clickOn("//input[@value='Continue']");
+        log.info("Submitted the email to reset the password.");
+
+        // Verify that the success message is displayed
+        WebElement successMessage = driver.findElement(By.xpath("//div[contains(@class,'alert-success')]"));
+        String actualSuccessMessage = successMessage.getText();
+        String expectedSuccessMessage = "An email with a confirmation link has been sent your email address.";
+        assertEquals(actualSuccessMessage, expectedSuccessMessage);
+    }
+
+    //verify resetting password for non registred users
+    @Test
+    public void resetPassForNonRegistredUsers() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the forgot password page
+        clickOn("//a[normalize-space()='Forgotten Password']");
+        log.info("Clicked on the 'Forgotten Password' link success");
+
+        // Enter the non registered email and  click on submit
+        type("#input-email","abc123@gmail.com" );
+        clickOn("//input[@value='Continue']");
+        log.info("Submitted the email to reset the password.");
+
+        // Retrieve the confirmation message
+        WebElement confirmationMessage = driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        String actualConfirmationMessage = confirmationMessage.getText();
+        String expectedErrorMessage="unregistred Email";
+        // Assert that the confirmation message matches the expected error message
+        assertEquals(actualConfirmationMessage,expectedErrorMessage);
+
+    }
+
+    //verify the resetting password without providing an email
+    @Test
+    public void resetPasswordWithoutEmail() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the forgot password page
+        clickOn("//a[normalize-space()='Forgotten Password']");
+        log.info("Clicked on the 'Forgotten Password' link success");
+        clickOn("//input[@value='Continue']");
+
+        //assert that the email message is displayed
+        String expectedErrorMessage = "Warning: The E-Mail Address was not found in our records, please try again!";
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']"));
+        String actualErrorMessage = errorMessage.getText();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+
+    }
+    //verify placehold text of email address is displayed
+    @Test
+    public void verifyThePlaceHoldText() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the forgot password page
+        clickOn("//a[normalize-space()='Forgotten Password']");
+        log.info("Clicked on the 'Forgotten Password' link success");
+
+        //verify the placehold text of EMail address
+        WebElement emailInput = driver.findElement(By.xpath("//input[@id='input-email']"));
+        String actualPlaceholder = emailInput.getAttribute("placeholder");
+        String expectedPlaceholder = "E-Mail Address";
+        assertEquals(actualPlaceholder, expectedPlaceholder);
+
+
+    }
+    //verify the back button of the reset password page
+
+    @Test
+    public void verifyBackButtonOnForgotPassword() {
+        // Navigate to the Forgot Password page
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the forgot password page
+        clickOn("//a[normalize-space()='Forgotten Password']");
+        log.info("Clicked on the 'Forgotten Password' link success");
+
+        // Click on the Back button
+        clickOn(".pull-left > a");
+
+
+        // Verify that the user is taken back to the Login page
+        String expectedTitle = "Account Login";
+        String actualTitle = driver.getTitle();
+        assertEquals(actualTitle, expectedTitle);
+        log.info("the user is taken back to the login page");
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/Rezika/SearchProducts.java
@@ -0,0 +1,147 @@
+package com.ninjaTutorials;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import us.piit.SetUp;
+
+public class SearchProducts  extends SetUp {
+
+    Logger log = LogManager.getLogger( SearchProducts.class.getName());
+
+
+    @Test
+    public void verifySearchWithAnExistingProduct() {
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Find the search field and enter the search term
+        type("//input[@name='search']","MacBook");
+        log.info("the search product enters in success");
+
+        // Find and click the search button
+        clickOn("button[class='btn btn-default btn-lg']");
+        log.info("the click button clicked in success");
+
+
+        // Verify that the search results page is displayed
+        WebElement searchResultsTitle = driver.findElement(By.cssSelector("#content h1"));
+        assertEquals(searchResultsTitle.getText(), "Search - MacBook");
+
+        // Verify that the search results contain at least one product
+        List<WebElement> productItems = driver.findElements(By.cssSelector(".product-layout .product-thumb"));
+        assertTrue(productItems.size() > 0, "Search results contain at least one product");
+    }
+    @Test
+
+    public void verifySearchWithANonExistingProduct() {
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Find the search field and enter the search term
+        type("//input[@name='search']","headphones");
+        log.info("the search product enters in success");
+
+        // Find and click the search button
+        clickOn("button[class='btn btn-default btn-lg']");
+        log.info("the click button clicked in success");
+
+
+        // Verify that the search results page is displayed
+        WebElement searchResultsTitle = driver.findElement(By.cssSelector("#content h1"));
+        assertEquals(searchResultsTitle.getText(), "Search - headphones");
+
+        //assert a non exsisting product message is displayed
+
+        // Find the error message element
+        WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"content\"]/p[2]"));
+        // Assert the error message text
+        assertEquals(errorMessage.getText(), "There is no product that matches the search criteria.");
+
+    }
+
+    //verify if the user is able to sort the products
+    @Test
+    public void verifySortingProducts() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Find the search field and enter the search term
+        type("//input[@name='search']","MacBook");
+        log.info("the search product enters in success");
+
+        // Find and click the search button
+        clickOn("button[class='btn btn-default btn-lg']");
+        log.info("the click button clicked in success");
+
+        // Find the sort by dropdown element and click on it
+        clickOn ("//select[@id='input-sort']");
+
+        // Verify that the products are sorted by price in ascending order
+        // Find the sort by dropdown
+        WebElement sortByDropdown = driver.findElement(By.id("input-sort"));
+        // Create a new Select object
+        Select select = new Select(sortByDropdown);
+
+        // Select an option by visible text
+        select.selectByVisibleText("Price (High > Low)");
+
+        log.info("the user can sort product in success");
+
+
+    }
+    //verify the user able to select how many products to be displayed
+    @Test
+
+    public void verifyProductDisplayCount() {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Find the search field and enter the search term
+        type("//input[@name='search']","MacBook");
+        log.info("the search product enters in success");
+
+        // Find and click the search button
+        clickOn("button[class='btn btn-default btn-lg']");
+        log.info("the click button clicked in success");
+
+        // Find the display count dropdown element and click on it
+        clickOn ("//select[@id='input-limit']");
+
+        // Verify that the display count options are displayed
+        WebElement displayCountDropdown = driver.findElement(By.id("input-limit"));
+        Select select = new Select(displayCountDropdown);
+
+        List<WebElement> options = select.getOptions();
+        assertTrue(options.size() > 1, "Display count options are not displayed");
+
+        // Select an option by visible text
+        select.selectByVisibleText("100");
+
+        // Verify that the number of displayed products matches the selected count
+        List<WebElement> productTitles = driver.findElements(By.cssSelector(".product-layout .product-thumb h4 a"));
+        assertEquals(productTitles.size(), 100, "Displayed product count doesn't match the selected count");
+
+        log.info("the user can select how many products to display in success");
+
+
+
+
+
+
+
+    }
+
+
+
+}
