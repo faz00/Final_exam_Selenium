865cde14360c90fd29af6757c01f220b7e6d6185
Laique <jamallaique24@gmail.com>
2023-05-23 19:42:21 +0500

    Revert "Boris new tests, data provider"
    This reverts commit 09f4de0ec60bcf86c642e3de2ec82a79ee718d46.

--- src/test/java/luma/LoginTest.java
+++ src/test/java/luma/LoginTest.java
@@ -3,7 +3,6 @@ package luma;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import us.piit.base.CommonAPI;
 import us.piit.pages.luma.LoginPage;
@@ -19,16 +18,9 @@ public class LoginTest extends CommonAPI {
     String ValidEmail = prop.getProperty("luma.username");
     String validPassword = prop.getProperty("luma.password");
 
-    @DataProvider(name = "validLoginData")
-    public Object[][] getValidLoginData(){
-        return new Object[][]{
-                {ValidEmail, validPassword}
-        };
-    }
 
-
-    @Test(priority = 0, groups = "Login", dataProvider = "validLoginData")
-    public void loginWithValidCredentials(String valid_email, String valid_password){
+    @Test
+    public void loginWithValidCredentials(){
         LoginPage loginPage = new LoginPage(getDriver());
 
         // Verify that user is on Home page
@@ -40,8 +32,8 @@ public class LoginTest extends CommonAPI {
         loginPage.goToLoginPage();
 
         //enter  username, password, and click on login button
-        loginPage.enterUsername(valid_email);
-        loginPage.enterPassword(valid_password);
+        loginPage.enterUsername(ValidEmail);
+        loginPage.enterPassword(validPassword);
         loginPage.clickOnSignInBtn();
 
         // verify that user is logged in
