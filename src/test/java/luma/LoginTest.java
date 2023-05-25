6877fd4069d0e498e4a7ad8fa8cf146d5e82e169
Boris <boris.abunagimov@gmail.com>
2023-05-25 16:52:26 +0500

    final_exam_selenium

--- src/test/java/luma/LoginTest.java
+++ src/test/java/luma/LoginTest.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import us.piit.base.CommonAPI;
 import us.piit.pages.luma.LoginPage;
@@ -26,6 +27,13 @@ public class LoginTest extends CommonAPI {
         };
     }
 
+    @DataProvider(name = "inValidLoginData")
+    public Object[][] getInValidLoginData(){
+        return new Object[][]{
+                {"invalid@gmail.com", "invalidpassword"}
+        };
+    }
+
     @Test(priority = 0, groups = "Login", dataProvider = "validLoginData")
     public void loginWithValidCredentials(){
         LoginPage loginPage = new LoginPage(getDriver());
@@ -45,5 +53,47 @@ public class LoginTest extends CommonAPI {
 
         // verify that user is logged in
         Assert.assertTrue(loginPage.verifyUserIsLoggedIn());
+    }
+
+    @Test(priority = 0, groups = "Login", dataProvider = "inValidLoginData")
+    public void loginWithInValidCredentials(String invalid_email, String invalid_password){
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        // Verify that user is on Home page
+        String actualTitle = getCurrentTitle();
+        Assert.assertEquals(actualTitle, "Home Page");
+        waitFor(3);
+
+        // go to login page
+        loginPage.goToLoginPage();
+
+        //enter  username, password, and click on login button
+        loginPage.enterUsername(invalid_email);
+        loginPage.enterPassword(invalid_password);
+        loginPage.clickOnSignInBtn();
+
+        // verify that user is logged in
+        Assert.assertTrue(loginPage.verifyValidationError());
+    }
+
+    @Test
+    public void loginWithEmptyCredentials(){
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        // Verify that user is on Home page
+        String actualTitle = getCurrentTitle();
+        Assert.assertEquals(actualTitle, "Home Page");
+        waitFor(3);
+
+        // go to login page
+        loginPage.goToLoginPage();
+
+        //enter  username, password, and click on login button
+        loginPage.enterUsername("");
+        loginPage.enterPassword("");
+        loginPage.clickOnSignInBtn();
+
+        // verify that user is logged in
+        Assert.assertTrue(loginPage.verifyFieldErros());
     }
 }
