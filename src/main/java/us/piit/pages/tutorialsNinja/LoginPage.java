7ef3c1fdf70941ddff6d5bd97acddaf5b7282015
Fazia Sid Ali <79808990+faz00@users.noreply.github.com>
2023-05-29 16:25:13 -0600

    LoginPage.java

--- src/main/java/us/piit/pages/tutorialsNinja/LoginPage.java
+++ src/main/java/us/piit/pages/tutorialsNinja/LoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 import us.piit.base.CommonAPI;
 
 public class LoginPage extends CommonAPI {
