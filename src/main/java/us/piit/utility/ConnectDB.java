2fcfae7c71307d1d4b217fd1814ac1b8ab5baf12
Fazia Sid Ali <79808990+faz00@users.noreply.github.com>
2023-05-29 16:29:58 -0600

    Update ConnectDB.java

--- src/main/java/us/piit/utility/ConnectDB.java
+++ src/main/java/us/piit/utility/ConnectDB.java
@@ -1,104 +1,70 @@
-
 package us.piit.utility;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import us.piit.utility.Utility;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-public class ExcelReader {
-    private final Logger LOG = LogManager.getLogger(ExcelReader.class.getName());
+public class ConnectDB {
 
-    XSSFWorkbook excelWBook;
-    XSSFSheet excelWSheet;
-    XSSFCell cell;
-    String path;
+    public static Connection connect = null;
+    public static Statement statement = null;
+    public static PreparedStatement ps = null;
+    public static ResultSet resultSet = null;
 
-    public ExcelReader(String path){
-        this.path = path;
-    }
+    static Properties prop = Utility.loadProperties();
 
-    public String getDataFromCell(String sheet, int rowNum, int colNum){
+    public static Connection connectToSqlDatabase() {
+
+        String driverClass = prop.getProperty("MYSQLJDBC.driver");
+        String url = prop.getProperty("MYSQLJDBC.url");
+        String userName = prop.getProperty("MYSQLJDBC.userName");
+        String password = prop.getProperty("MYSQLJDBC.password");
         try {
-            FileInputStream excelFile = new FileInputStream(path);
-            excelWBook = new XSSFWorkbook(excelFile);
-            excelWSheet = excelWBook.getSheet(sheet);
-            cell = excelWSheet.getRow(rowNum).getCell(colNum);
-            String cellValue = cell.getStringCellValue();
-            excelFile.close();
-            return cellValue;
-        }catch (Exception e){
-            LOG.info("no file found");
-            return "";
+            Class.forName(driverClass);
+            connect = DriverManager.getConnection(url,userName,password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        System.out.println("Database is connected");
+        return connect;
     }
 
-    public List<String> getEntireColumnData(String sheet, int rowStart, int colNum){
-        List<String> columnData = new ArrayList<>();
+    public static MongoDatabase mongoDatabase = null;
+
+    public MongoDatabase connectToMongoDB() {
+        MongoClient mongoClient = new MongoClient("" , 27017);
+        mongoDatabase = mongoClient.getDatabase("");
+        System.out.println("Database Connected");
+
+        return mongoDatabase;
+    }
+
+    public static List<String> getTableColumnData(String query, String columnName) {
+        List<String> list = new ArrayList<>();
         try {
-            File file = new File(path);
-            FileInputStream excelFile = new FileInputStream(file);
-            excelWBook = new XSSFWorkbook(excelFile);
-            excelWSheet = excelWBook.getSheet(sheet);
-            for (int i = rowStart; i <= excelWSheet.getLastRowNum(); i++){
-                columnData.add(excelWSheet.getRow(i).getCell(colNum).getStringCellValue());
+            statement = connectToSqlDatabase().createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                list.add(resultSet.getString(columnName));
             }
-//            int i = rowStart;
-//            while (excelWSheet.getRow(i).getCell(colNum).getStringCellValue() != null){
-//                columnData.add(excelWSheet.getRow(i).getCell(colNum).getStringCellValue());
-//                i++;
-//            }
-            excelFile.close();
-        }catch (Exception e){
-            e.printStackTrace();
-            LOG.info("no data found");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return columnData;
+        return list;
     }
 
-    public List<String> getEntireColumnForGivenHeader(String sheet, String headerName){
-        int i = 0;
-        while (getDataFromCell(sheet, 0, i) != null){
-            if(getDataFromCell(sheet, 0, i).equalsIgnoreCase(headerName)){
-                getEntireColumnData(sheet, 1, i);
-                break;
-            }
-            i++;
-        }
-        return getEntireColumnData(sheet, 1, i);
-    }
+    public static void main(String[] args) throws SQLException {
 
-    public String getValueForGivenHeaderAndKey(String sheet, String headerName, String key){
-        String value = null;
-        int i = 0;
-        while (getDataFromCell(sheet, 0, i) != null){
-            if(getDataFromCell(sheet, 0, i).equalsIgnoreCase(headerName)){
-                for (int j = 0; j < getEntireColumnData(sheet, 1, i).size(); j++){
-                    if(getEntireColumnData(sheet, 1, i).get(j).equalsIgnoreCase(key)){
-                        value = getEntireColumnData(sheet, 1, i+1).get(j);
-                    }
-                }
-                break;
-            }
-            i++;
-        }
-        return value;
-    }
 
-    public static void main(String[] args)  {
 
-        String path = "C:\\Users\\My Pc\\eclipse-workspace\\Final_exam_Selenium\\manualTestCases\\tutorialsNinja-TestCases.xlsx";
-        ExcelReader excelReader = new ExcelReader(path);
-        System.out.println(excelReader.getDataFromCell("TutorialsNinjaWebSite-TestCases",1,1));
+      List<String> password = getTableColumnData("select * from orangeHRM;","validCodedNewPassword");
+      System.out.println(password.get(0));
 
-//        List<String> items = excelReader.getEntireColumnForGivenHeader("Sheet1", "id");
-//        //String items = excelReader.getValueForGivenHeaderAndKey("Sheet1", "id", "id004");
-//        System.out.println(items);
+
     }
 }
